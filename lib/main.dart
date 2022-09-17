9d12c43ef1973953111232b06249c227ab161772
hafeezarfi <hafeezarfi@gmail.com>
2022-09-18 00:15:07 +0530

    state with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,173 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(ChangeNotifierProvider(
+    create: (_) => BreadcrumbProvider(),
+    child: MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
+      routes: {
+        '/new': (context) => const NewBreadcrumbWidget(),
+      },
+      debugShowCheckedModeBanner: false,
+    ),
+  ));
+}
+
+class Breadcrumb {
+  bool isActive;
+  final String name;
+  final String uuid;
+
+  Breadcrumb({
+    required this.isActive,
+    required this.name,
+  }) : uuid = const Uuid().v4();
+
+  void active() {
+    isActive = true;
+  }
+
+  @override
+  bool operator ==(covariant Breadcrumb other) => uuid == other.uuid;
+
+  @override
+  int get hashCode => uuid.hashCode;
+
+  String get title => name + (isActive ? '> ' : '');
+}
+
+class BreadcrumbProvider extends ChangeNotifier {
+  final List<Breadcrumb> _items = [];
+  UnmodifiableListView<Breadcrumb> get items => UnmodifiableListView(_items);
+
+  void add(Breadcrumb breadcrumb) {
+    for (final item in _items) {
+      item.active();
+    }
+    _items.add(breadcrumb);
+    notifyListeners();
+  }
+
+  void reset() {
+    _items.clear();
+    notifyListeners();
+  }
+}
+
+class BreadcrumbWidget extends StatelessWidget {
+  final UnmodifiableListView<Breadcrumb> breadcrumbs;
+  const BreadcrumbWidget({super.key, required this.breadcrumbs});
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: breadcrumbs.map((breadcrumb) {
+        return Text(
+          breadcrumb.title,
+          style: TextStyle(
+            color: breadcrumb.isActive ? Colors.blue : Colors.black,
+          ),
+        );
+      }).toList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Column(
+        children: [
+          Consumer<BreadcrumbProvider>(
+            builder: (context, value, child) {
+              return BreadcrumbWidget(
+                breadcrumbs: value.items,
+              );
+            },
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                '/new',
+              );
+            },
+            child: const Text('Add new breadcrumb'),
+          ),
+          TextButton(
+            onPressed: () {
+              context.read<BreadcrumbProvider>().reset();
+            },
+            child: const Text('Reset'),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class NewBreadcrumbWidget extends StatefulWidget {
+  const NewBreadcrumbWidget({super.key});
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  State<NewBreadcrumbWidget> createState() => _NewBreadcrumbWidgetState();
+}
+
+class _NewBreadcrumbWidgetState extends State<NewBreadcrumbWidget> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Add new breadcrumb'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              hintText: 'Enter a new breadcrumb here',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+          TextButton(
+            onPressed: () {
+              final text = _controller.text;
+              if (text.isNotEmpty) {
+                final breadcrumb = Breadcrumb(isActive: false, name: text);
+                context.read<BreadcrumbProvider>().add(breadcrumb);
+                Navigator.of(context).pop();
+              }
+            },
+            child: const Text('Add'),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
